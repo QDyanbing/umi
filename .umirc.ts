3afce19f99ff0cfc4a88715ee1e2c163de0efd8d
高艳兵 <yanbing.gao@aliyun.com>
2022-09-09 19:47:41 +0800

    feat: qiankun

--- .umirc.ts
+++ .umirc.ts
@@ -10,25 +10,11 @@ export default defineConfig({
     title: '@umijs/max',
   },
   routes: [
-    {
-      path: '/',
-      redirect: '/home',
-    },
-    {
-      name: '首页',
-      path: '/home',
-      component: './Home',
-    },
-    {
-      name: '权限演示',
-      path: '/access',
-      component: './Access',
-    },
-    {
-      name: ' CRUD 示例',
-      path: '/table',
-      component: './Table',
-    },
+    { path: '/', redirect: '/hello' },
+    { path: '/hello', component: './Hello' },
   ],
+  qiankun: {
+    slave: {},
+  },
   npmClient: 'yarn',
 });
---
+++ src/pages/Hello/index.tsx
@@ -0,0 +1,7 @@
+import styles from './index.less';
+
+const HomePage: React.FC = () => {
+  return <div className={styles.container}>hello word</div>;
+};
+
+export default HomePage;
